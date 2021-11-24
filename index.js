1a4936ac31e0a53c9be31ab221a2dd437adb57ad
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-11-24 22:32:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const canvas = document.getElementById("myCanvas");
+const cxt = canvas.getContext("2d");
+
+cxt.fillStyle = "red";
+cxt.fillRect(5,30,1,80)
+cxt.fillRect(7,30,1,80)
+// cxt.fillRect(5,15,1,80)
