580d24fba5ffc6278da666a105aa51e1fc49692f
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-11-24 23:12:18 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 
 const canvas = document.getElementById("myCanvas");
 const cxt = canvas.getContext("2d");
+const heights = [30,50,20,60,15,25,35];
+const index = [45,55,65];
+const random = ()=>{
+    return heights[Math.floor(Math.random()*(heights.length-1))];
+}
+const random1 = ()=>{
+     return index[Math.floor(Math.random()*(index.length-1))];
+
+}
 
 cxt.fillStyle = "red";
-cxt.fillRect(5,30,1,80)
-cxt.fillRect(7,30,1,80)
-// cxt.fillRect(5,15,1,80)
+let c=5;
+
+function fillRect(){
+    for(var i=0; i<145; i++){
+        var ran = random();
+        var ran1 = random1();
+        cxt.fillRect(c,index,1,ran)
+        c+=2;
+        console.log("object")
+    }
+}
+fillRect();
